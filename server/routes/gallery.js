d93345c2afcb078500487c15a2cf794b35f6f81e
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-07-26 13:06:56 +0530

    galleryConnectedToServer

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { fetchGallery,uploadImages} from "../controllers/gallery.js";
+import { fetchGallery,uploadImages,fetchPageCount} from "../controllers/gallery.js";
 
 const router=express.Router();
 
 router.get("/",fetchGallery);
+router.get("/pageCount",fetchPageCount);
 router.post("/uploadImages",uploadImages);
 
 export default router;
