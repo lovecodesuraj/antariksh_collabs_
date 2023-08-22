c13e40dfbfcf54eccbf9f39d989d0d47ce5743e6
Garvit <garvitg0108@gmail.com>
2023-08-23 00:22:38 +0530

    Add Member

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
-import {Button} from "@mui/material";
-import {useDispatch,useSelector} from "react-redux";
+import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { uploadImages } from '../../actions/gallery';
 import AddImage from './components/addImage';
+import AddMember from './components/addImage/addMember/addMember';
 
 const Admin = () => {
-  
-
-
-    return (
-        <>
-        <AddImage />
-     </>
+  return (
+    <>
+      <AddImage />
+      <AddMember/>
+    </>
   )
 }
 
