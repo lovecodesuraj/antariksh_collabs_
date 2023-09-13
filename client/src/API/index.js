c8451c1820b0f18bc63e5fb840ddcdf1467bf0ce
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-09-14 03:10:13 +0530

    contact_activities_rrsponsiveHeader_added_

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -3,12 +3,7 @@ import axios from "axios";
 const API=axios.create({baseURL:"https://astrons-server.onrender.com/"});
 // const API=axios.create({baseURL:"http://localhost:5000"});
 
-// API.interceptors.request.use((req)=>{
-//     if(localStorage.getItem('profile')){
-//         req.headers.authorization= `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-//     }
-//     return req;
-// })
+
 
 //gallery
 export const uploadImages=(data)=>API.post(`/gallery/uploadImages`,data);
