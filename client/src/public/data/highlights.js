1cd43ce23a7c17af09edd6f2cec906c6400eddfa
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-09-14 11:34:54 +0530

    build_fixed

---
+++ client/src/public/data/highlights.js
@@ -0,0 +1,10 @@
+export const highlights=[{
+    image:"https://imgs.search.brave.com/1l-NNHZMxI0hGUYevlkiX1DxU2_xAi_TKpQPxaaagtE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9h/c3Ryb25hdXQtZXhw/bG9yZXMtb3V0ZXIt/ZGFyay1zcGFjZS1n/ZW5lcmF0aXZlLWFs/XzE2OTAxNi0yODYw/Ny5qcGc_c2l6ZT02/MjYmZXh0PWpwZw",
+    title:"welcome to antariksh",
+    publishTime:"15 sept 2023"
+  },
+  { image:"https://imgs.search.brave.com/qvHR8_vtuebt7Il1QGyg5f8qVq4pu1m-5s0iij_F4nw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE0/NjIzMzI0MjA5NTgt/YTA1ZDFlMDAyNDEz/P2l4bGliPXJiLTQu/MC4zJml4aWQ9TTN3/eE1qQTNmREI4TUh4/elpXRnlZMmg4T1h4/OFlYTjBjbTl1YjIx/NWZHVnVmREI4ZkRC/OGZId3cmdz0xMDAw/JnE9ODA.jpeg",
+    title:"welcome to antariksh",
+    publishTime:"15 sept 2023"
+  }
+]
