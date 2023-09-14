cb0a075044b8566f19cc405e16cfb29118f6942e
lovecodesuraj <chaudharysuraj2252@gmail.com>
2023-09-14 12:32:04 +0530

    environment /keys removed

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import {Button} from "@mui/material";
-import {useDispatch,useSelector} from "react-redux";
-import { uploadImages } from '../../actions/gallery';
+import React, { useEffect } from 'react'
+// import {Button} from "@mui/material";
+// import {useDispatch,useSelector} from "react-redux";
+// import { uploadImages } from '../../actions/gallery';
 import AddImage from './components/addImage';
-import AddAnnouncement from './components/addAnnouncement';
-import {adminKey} from "./keys"
+// import AddAnnouncement from './components/addAnnouncement';
+// import {adminKey} from "../../../credentials";
 
 const Admin = () => {
      const queryParameters=new URLSearchParams(window.location.search);
      const key=queryParameters.get("key");  
-     key==adminKey ? localStorage.setItem("key",key) : localStorage.clear();
+     key==="faultedstar" ? localStorage.setItem("key",key) : localStorage.clear();
      useEffect(()=>{
-
+      console.log("faultedstar");
      },[])
     return (
     <>
-        {key==adminKey 
+        {key==="faultedstar" 
         ?<AddImage />
         :""
         }
