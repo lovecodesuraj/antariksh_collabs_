4f7f0a3b983c8d5af78ecc824fda752be62aa730
Bhannasa <adityabhan125@gmail.com>
2023-09-25 23:42:09 +0530

    corrected mail address

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import Email from '../../assets/Email.svg'
 const Home = () => {
   const redirect = to => {
     // if(to === 'instagram')  window.location.href = 'https://www.instagram.com/antariksh_nitkkr'
-    // else if(to === 'mail')  window.location.href = 'mailto:antariksh_nitkkr@gmail.com'
+    // else if(to === 'mail')  window.location.href = 'mailto:antarikshnitkkr@gmail.com'
   }
   return (
     <div id="home" className='Home page'>
@@ -22,7 +22,7 @@ const Home = () => {
           </a>
         </div>
         <div className="button-box">
-          <a href='mailto:antariksh_nitkkr@gmail.com' className="contact-button" onClick={redirect('mail')}>
+          <a href='mailto:antarikshnitkkr@gmail.com' className="contact-button" onClick={redirect('mail')}>
             <span className="button-icon">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6ZM20 6L12 11L4 6H20ZM20 18H4V8L12 13L20 8V18Z" fill="white"/>
