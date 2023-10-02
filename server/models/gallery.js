f3aa51dcdd83d99e605b757ebeecd9be3e7453ce
Ashish Saini <ashu78021@gmail.com>
2023-10-02 11:55:14 +0530

    gallery issue is temporary fixed

--- server/models/gallery.js
+++ server/models/gallery.js
@@ -1,28 +1,30 @@
 import mongoose from "mongoose";
 
-// const imageSchema=mongoose.Schema({
-//      createdBy:String,
-//      picture:String,
-//      date:String,
-//      name: String,
-//      forGallery: { type: Boolean, default: true}
-// });
+const imageSchema=mongoose.Schema({
+     createdBy:String,
+     picture:String,
+     date:String,
+     name: String,
+     forGallery: { type: Boolean, default: true}
+});
 
-// const Image=mongoose.model("Image",imageSchema);
-const imageSchema = new mongoose.Schema({
-     title :{
-         type : String ,
-         required : true
-     } ,
-     image : {
-         type : String
-     } ,
-     price : {
-         type : Number ,
-         min : 1 , 
-         default : 10
-     }
- });
 const Image=mongoose.model("Image",imageSchema);
+
+// It is for testing purpose
+// const imageSchema = new mongoose.Schema({
+//      title :{
+//          type : String ,
+//          required : true
+//      } ,
+//      image : {
+//          type : String
+//      } ,
+//      price : {
+//          type : Number ,
+//          min : 1 , 
+//          default : 10
+//      }
+//  });
+// const Image=mongoose.model("Image",imageSchema);
 
 export default Image;
--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -4,22 +4,6 @@ import mongoose from "mongoose";
 import { fetchGallery,deleteImage,uploadImages,fetchPageCount, fetchImage} from "../controllers/gallery.js";
 
 const router=express.Router();
-
-// temp starts---->>>
-main()
-.then(()=> {
-    console.log("connection successfull");
-})
-.catch((err)=> {
-    console.log(err);
-})
-
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/amazon"); // it will wait for the promise
-}
-
-
-
 router.get("/",fetchGallery);
 router.get("/pageCount",fetchPageCount);
 router.get("/image",fetchImage);
@@ -27,6 +11,6 @@ router.post("/uploadImages",uploadImages);
 router.delete('/delete',deleteImage);
 
 
-// temp ends--->>>
+
 
 export default router;
